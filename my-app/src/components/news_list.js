fb7e4becd2410872a280ef378c55d72b2ccbf570
genie-ru <genie.zgmf987@gmail.com>
2023-06-04 10:56:10 +0900

    add props

---
+++ my-app/src/components/news_list.js
@@ -0,0 +1,10 @@
+const Newslist = (props) => {
+    console.log(props);
+    return (
+        <>
+            <div>News list</div>
+        </>
+    )
+}
+
+export default Newslist;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css'
-
+import Newslist from './components/news_list';
 
 import Header from './components/header';
+import JSON from './db.json';
 
+class App extends Component {
 
-const App = () => {
-    return(
-        <div className="hey">
-            <Header/>
-        </div>
-    )
+    state = {
+        news:JSON,
+    }
+    render(){
+        console.log(this.state.news)
+        return(
+            <div className="hey">
+                <Header/>
+                <Newslist
+                    news={this.state.news}/>
+            </div>        
+        )
+    }
 }
+
+// const App = () => {
+
+    
+//     return(
+//         <div className="hey">
+//             <Header/>
+//         </div>
+//     )
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

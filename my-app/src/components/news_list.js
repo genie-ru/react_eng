15dec790d64ab311fc2889398f18437be4b0e9a4
genie-ru <genie.zgmf987@gmail.com>
2023-06-05 19:46:42 +0900

    add prop2

--- my-app/src/components/news_list.js
+++ my-app/src/components/news_list.js
@@ -1,10 +1,18 @@
-const Newslist = (props) => {
-    console.log(props);
+import NewsListItem from "./news_list_item";
+
+
+const NewsList = (props) => {
+
+    const news = props.news.map((item)=>(
+        <NewsListItem key={item.id} item={item}/>
+    ))   
+
+
     return (
         <>
-            <div>News list</div>
+           { news } 
         </>
     )
 }
 
-export default Newslist;
+export default NewsList;
